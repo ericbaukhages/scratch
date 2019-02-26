d5fec7f7a09ef6648ed709d4711ffd4d88de6c6c
Eric Baukhages <febvigrail@gmail.com>
2019-02-26 15:04:40 -0500

    init

---
+++ scratchtime.sh
@@ -0,0 +1,48 @@
+#!/bin/bash
+
+if [[ "$#" -ne 1 ]]; then
+  echo "Usage: $0 [input.md]"
+  exit 1
+fi
+
+INPUT="$1"
+
+if [[ ! -e $INPUT ]]; then
+  echo "$INPUT does not exist"
+  exit 1
+fi
+
+outputTime()
+{
+  timeInSeconds=$1
+
+  hours=$(($timeInSeconds / 3600 ))
+  minutes=$((($timeInSeconds % 3600) / 60))
+  seconds=$(($timeInSeconds % 60 ))
+  
+  echo "${hours}h ${minutes}m ${seconds}s"
+}
+
+echo "==== TIME ===="
+
+totalSeconds=0
+
+IFS=$'
+'
+for date in `pandoc -t html $INPUT | pup 'h2 json{}' | jq '.[] | .text' -r | sed 's/\(.*\) - \(.* [ap]m\) to \(.* [ap]m\)/{"title":"\1","start":"\2","end":"\3"}/g'`; do
+  title=`echo $date | jq -r '.title'`
+  start=`echo $date | jq -r '.start'`
+  end=`echo $date | jq -r '.end'`
+
+  startDate=`date -j -f "%I:%M %p" "$start" "+%s"`
+  endDate=`date -j -f "%I:%M %p" "$end" "+%s"`
+
+  diff=$(($endDate - $startDate)) 
+  totalSeconds=$(($totalSeconds + $diff))
+
+  echo " - $title: `outputTime $diff`"
+done
+unset IFS
+
+echo "==== TOTAL ===="
+echo "Total time tracked for \"$INPUT\": `outputTime $totalSeconds`"
