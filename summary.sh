1b6a414d79b8d5ce9bf755be46aa6fecfd45a549
Eric Baukhages <febvigrail@gmail.com>
2019-05-07 13:49:03 -0400

    update SCRIPT_DIR

--- summary.sh
+++ summary.sh
@@ -3,6 +3,7 @@
 SCRATCH_DIR=~/.scratch
 
 SCRATCH_PREV_DIR=`pwd`
+SCRIPT_DIR=`dirname $0`
 
 cd $SCRATCH_DIR
 
@@ -10,7 +11,7 @@ FILENAME="`date +"%Y-%m-%d"`.md"
 
 # Save the summary headers to a temporary file
 TEMPHEADERS=`mktemp`
-`dirname $0`/index.js > $TEMPHEADERS
+$SCRIPT_DIR/summary/index.js > $TEMPHEADERS
 
 if [ -f $FILENAME ]; then
   vim $FILENAME && cd $SCRATCH_PREV_DIR
